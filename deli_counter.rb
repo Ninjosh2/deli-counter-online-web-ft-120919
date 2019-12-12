2720ca7c832033f8f8e856c80139d2b1964a6d15
Joshua Sanders <ninjosh2@gmail.com>
2019-12-12 23:12:24 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,17 @@
-# Write your code here.
+def line(line_array)
+  if line_array.length == 0 
+    puts "The line is currently empty."
+  else
+    output_string = "The line is currently:"
+    line_array.each.with_index(1) do |name, index|
+      output_string << " #{index}. #"name}"
+    end
+    puts output_string
+  end
+      
+    
+  
+  
+  
+  
+end
