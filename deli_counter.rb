a53de29212481a36f52b6d72ff070d5f76d0a44a
Joshua Sanders <ninjosh2@gmail.com>
2019-12-12 23:52:28 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,6 +3,8 @@ def line(line_array)
   if line_array.length == 0 
     puts "The line is currently empty."
   else
+    
+    
     output_string = "The line is currently:"
     line_array.each.with_index(1) do |name, i|
       output_string << " #{i}. #{name}"
