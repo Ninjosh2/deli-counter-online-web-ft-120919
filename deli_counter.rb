8c79fd65b3a3f075eeeb628aeacfd0449aa9adde
Joshua Sanders <ninjosh2@gmail.com>
2019-12-13 00:30:49 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -11,7 +11,7 @@ def line(line_array)
     end
     puts output_string
   end
-
+end 
 def take_a_number(deli, name)
   deli << name
   puts "Welcome, #{name}. You are number #{deli.length} in line."
